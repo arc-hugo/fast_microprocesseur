094f6aeaf6937536c62ce73e96f9a7ae4e5c9de5
Hugo Barral <hugo.barral@outlook.fr>
2022-04-08 11:50:31 +0200

    correction ALU

--- fast_microprocesseur.srcs/sources_1/new/alu.vhd
+++ fast_microprocesseur.srcs/sources_1/new/alu.vhd
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity alu is
-    generic (Nb: Natural := 16);
+    generic (NB: Natural := 16);
     Port ( A : in STD_LOGIC_VECTOR (NB-1 downto 0);
            B : in STD_LOGIC_VECTOR (NB-1 downto 0);
            Ctrl_Alu : in STD_LOGIC_VECTOR (2 downto 0);
@@ -46,38 +46,35 @@ end alu;
 
 architecture Behavioral of alu is
 
-signal S_Plus: STD_LOGIC_VECTOR ((NB*2)+1 downto 0) := (others => '0');
-signal A_Plus: STD_LOGIC_VECTOR ((NB*2)+1 downto 0) := (others => '0');
-signal B_Plus: STD_LOGIC_VECTOR ((NB*2)+1 downto 0) := (others => '0');
+signal S_Plus: STD_LOGIC_VECTOR ((NB*2)-1 downto 0) := (others => '0');
 
 begin
 
 -- Opérations sur l'ALU
-S_Plus <= A_Plus + B_Plus when Ctrl_Alu = "000" else -- Addition
-          A_Plus - B_Plus when Ctrl_Alu = "001" else -- Soustraction
-          A_Plus * B_Plus when Ctrl_Alu = "010" else -- Multiplication
-          A_Plus and B_Plus when Ctrl_Alu = "011" else -- ET logique
-          A_Plus or B_Plus when Ctrl_Alu = "100" else -- OU logique
-          not A when Ctrl_Alu = "101"; -- NON logique
+S_Plus <= ((NB-1 downto 0 => '0') & A) + ((NB-1 downto 0 => '0')  & B) when Ctrl_Alu = "000" else -- Addition
+          ((NB-1 downto 0 => '0')  & A) - ((NB-1 downto 0 => '0')  & B) when Ctrl_Alu = "001" else -- Soustraction
+          A * B when Ctrl_Alu = "010" else -- Multiplication
+          ((NB-1 downto 0 => '0')  & A) and ((NB-1 downto 0 => '0')  & B) when Ctrl_Alu = "011" else -- ET logique
+          ((NB-1 downto 0 => '0')  & A) or ((NB-1 downto 0 => '0')  & B) when Ctrl_Alu = "100" else -- OU logique
+          not ((NB-1 downto 0 => '0')  & A) when Ctrl_Alu = "101"; -- NON logique
 
 -- Sortie de la valeur calculée
 S <= S_Plus(NB-1 downto 0);
 
 -- Gestion du Flag valeur négative (N)
-N <= '1' when Ctrl_Alu = "001" and B_Plus > A_Plus else
-     '1' when Ctrl_Alu = "010" and (A_Plus = (others => '0') or B_Plus = (others => '0')) else
+N <= '1' when Ctrl_Alu = "001" and B > A else
      '0';
 
 -- Gestion du Flag overflow multiplcation (O)
-O <= '1' when Ctrl_Alu = "010" and S_Plus((NB*2)+1 downto NB) > (others => '0') else
+O <= '1' when Ctrl_Alu = "010" and S_Plus((NB*2)-1 downto NB) > (NB-1 downto 0 => '0') else
      '0';
 
 -- Gestion du Flag valeur nulle (Z)
-Z <= '1' when S_Plus = (others => '0') else
+Z <= '1' when S_Plus = ((NB*2)-1 downto 0  => '0') else
      '0';
 
 -- Gestion du Flag carry addition (C)
-C <= '1' when Ctrl_Alu = "000" and S_Plus((NB*2)+1 downto NB) > (others => '0') else
+C <= '1' when Ctrl_Alu = "000" and S_Plus((NB*2)-1 downto NB) > (NB-1 downto 0 => '0') else
      '0';
 
 end Behavioral;
