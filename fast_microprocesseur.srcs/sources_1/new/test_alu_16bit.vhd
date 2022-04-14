52cc5d23bf6208de984818bf70e757885a8ab764
Hugo Barral <hugo.barral@outlook.fr>
2022-04-14 10:00:02 +0200

    nouveau test alu

--- fast_microprocesseur.srcs/sources_1/new/test_alu_16bit.vhd
+++ fast_microprocesseur.srcs/sources_1/new/test_alu_16bit.vhd
@@ -40,22 +40,43 @@ end test_alu_16bit;
 architecture Behavioral of test_alu_16bit is
 
 
+component alu
+generic (NB: Natural := 16);
+Port ( A : in STD_LOGIC_VECTOR (NB-1 downto 0);
+       B : in STD_LOGIC_VECTOR (NB-1 downto 0);
+       Ctrl_Alu : in STD_LOGIC_VECTOR (2 downto 0);
+       S : out STD_LOGIC_VECTOR (NB-1 downto 0);
+       N : out STD_LOGIC;
+       O : out STD_LOGIC;
+       Z : out STD_LOGIC;
+       C : out STD_LOGIC);
+end component;
 
-alu_16: alu port map (
+constant NB: integer := 16;
+signal A_local: STD_LOGIC_VECTOR (NB-1 downto 0) := (others => '0');
+signal B_local: STD_LOGIC_VECTOR (NB-1 downto 0) := (others => '0');
+signal Ctrl_local: STD_LOGIC_VECTOR (2 downto 0) := (others => '0');
+signal S_local: STD_LOGIC_VECTOR (NB-1 downto 0) := (others => '0');
+signal N_local: STD_LOGIC := '0';
+signal O_local: STD_LOGIC := '0';
+signal Z_local: STD_LOGIC := '0';
+signal C_local: STD_LOGIC := '0';
+
+begin
+
+alu_16: alu generic map (NB => NB) Port Map (
     A => A_local,
     B => B_local,
-    Ctrl_Alu => Ctrl_Alu_local,
+    Ctrl_Alu => Ctrl_local,
     S => S_local,
     N => N_local,
     O => O_local,
     Z => Z_local,
     C => C_local
 );
-begin
 
-end component;
-
-begin
-
+A_local <= X"EFFF" after 10ns;
+B_local <= X"0002" after 20ns;
+Ctrl_local <= "001" after 100ns, "010" after 200ns, "011" after 300ns, "100" after 400ns, "101" after 500ns;
 
 end Behavioral;
